a29cfac163a1b1fa102f4761e3aff06936ca4e7e
Ruhul Amin Sardar <84025740+using-namespace-ruhul@users.noreply.github.com>
2021-09-18 13:26:37 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (16.44%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.java
+++ search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,19 +1,47 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row_len = matrix[0].length;
-        int col_len = matrix.length;
+         //For searching the potential row:
+        int potential_row = searchForRow(matrix, target);
+        if(potential_row != -1){
+            return binarySearch(matrix[potential_row], target);
+        }
+    return false;
+    }
+    
+    public boolean binarySearch(int[] arr, int target){
+        int lo = 0;
+        int hi = arr.length-1;
         
-        int row = 0;
-        int col = row_len-1;
-        while(row<col_len && col>=0){
-            if(matrix[row][col]>target){
-                col--;
-            }else if(matrix[row][col]<target){
-                row++;
+        while( lo <= hi){
+            int mid = lo+(hi-lo)/2;
+            
+            if( target < arr[mid] ){
+                hi = mid-1;
+            }else if( target > arr[mid] ){
+                lo = mid+1;
             }else{
                 return true;
             }
         }
     return false;
+    }
+    
+    public int searchForRow(int[][] matrix, int target){
+        int s = 0;
+        int e = matrix.length-1;
+        int lc = matrix[0].length-1;
+        
+        while( s <= e){
+            int m = s+(e-s)/2;
+            
+            if( target>=matrix[m][0] && target<=matrix[m][lc]){//if target can lie in this row
+                return m;
+            }else if( target<matrix[m][0]){//If target lies above this row.
+                e = m-1;
+            }else if( target>matrix[m][0]){//if target lies beneath this row.
+                s = m+1;
+            }
+        }
+    return -1;//If target cannot exist in the matrix.
     }
 }
